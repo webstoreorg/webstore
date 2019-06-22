3477c9248e87aa3103fb96fce8bed30048e73ab6
SanDanCrazy <1017243931@qq.com>
2019-06-22 13:03:13 +0800

    update entity

--- shopping/src/main/java/com/summer/shopping/entity/Manager.java
+++
@@ -1,5 +0,0 @@
-package com.summer.shopping.entity;
-
-public class Manager {
-
-}
--- shopping/src/main/java/com/summer/shopping/entity/User.java
+++ shopping/src/main/java/com/summer/shopping/entity/User.java
@@ -1,19 +1,96 @@
 package com.summer.shopping.entity;
 
+import java.io.Serializable;
+import java.sql.Date;
 
-public class User {
+import com.summer.shopping.util.UserSexEnum;
 
-	private int id;
+public class User implements Serializable{
+	
+
+	private static final long serialVersionUID = 3301152882651495553L;
+
+	private int user_id;
 	
 	private String name;
+	
+	private String password;
+	
+	private UserSexEnum sex;
+	
+	private String phone;
+	
+	private String email;
+	
+	private Date birthday;
+	
+	private String picture;
+	
+	
 
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserSexEnum getSex() {
+		return sex;
+	}
+
+	public void setSex(UserSexEnum sex) {
+		this.sex = sex;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
 
 	public int getId() {
-		return id;
+		return user_id;
 	}
 
 	public void setId(int id) {
-		this.id = id;
+		this.user_id = id;
 	}
 
 	public String getName() {
@@ -27,6 +104,6 @@ public class User {
 	
 	@Override
 	public String toString() {
-		return "user{"+"id:"+id+",name:"+name+"}";
+		return "user{"+"id:"+user_id+",name:"+name+"}";
 	}
 }
